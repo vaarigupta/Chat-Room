d4cf7d13043959b0c2a9ff072123df2dc58445d4
vaarigupta <vaarigupta24@gmail.com>
2019-02-02 21:21:21 +0530

    initialization of App

--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 var publicPath = path.join(__dirname , '../public');
 var app = express();
+
+var PORT = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.use((req,res,next)=>{
-  res.send("<h1>Welcome to my page</h1>");
+  res.send("<h1>Welcome to Chat App</h1>");
   next();
 })
 
-app.listen(3000 , ()=>{
-  console.log("Started at 3000");
+app.listen(PORT , ()=>{
+  console.log(`up on port ${PORT}`);
 })
