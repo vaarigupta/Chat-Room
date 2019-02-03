8f25a38e8fae536d45d4d303d985b87055bab99c
vaarigupta <vaarigupta24@gmail.com>
2019-02-04 01:40:03 +0530

    socket emit at client

--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,16 @@
 var socketClient = io.connect('http://localhost:3000');
+
+var output = document.getElementById('output'),
+ user = document.getElementById('user'),
+ message = document.getElementById('message'),
+ but = document.getElementById('but');
+
+
+
+ but.addEventListener('click',function(socket)
+{
+  socketClient.emit('chat',{
+    message:message.value,
+    user : user.value
+  })
+})
