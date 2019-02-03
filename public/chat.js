3deacf4681f7157d60a7d986bf3ab6e348f81ced
vaarigupta <vaarigupta24@gmail.com>
2019-02-04 03:12:28 +0530

    broading started

--- public/chat.js
+++ public/chat.js
@@ -3,20 +3,21 @@ var socketClient = io.connect('http://localhost:3000');
 var output = document.getElementById('output'),
  user = document.getElementById('user'),
  message = document.getElementById('message'),
- but = document.getElementById('but');
+ but = document.getElementById('but'),
+ feedback=document.getElementById('feedback');
 
 
-
- but.addEventListener('click',function(socket)
-{
+but.addEventListener('click',function(){
   socketClient.emit('chat',{
     message:message.value,
-    user : user.value
+    user: user.value
   })
-  message : ""
 })
 
-
+message.addEventListener('keypress',function()
+{
+  socket.emit('typing',user.value);
+})
 socketClient.on('chat',function(data){
   output.innerHTML += "<p><b>"+ data.user + "</b> : " + data.message + "</p>" ;
 })
